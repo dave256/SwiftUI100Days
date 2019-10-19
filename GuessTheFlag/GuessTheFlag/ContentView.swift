7e266c4751414aa93da76ad33ccd642e48239e31
Dave Reed <dave256@mac.com>
2019-10-19 16:06:09 -0400

    GuessTheFlag: Wrap up Challenge 3
    https://www.hackingwithswift.com/books/ios-swiftui/guess-the-flag-wrap-up

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -62,7 +62,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, that's the flag of \(countries[number])"
         }
 
         showingScore = true
