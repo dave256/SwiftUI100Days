57e91be7b73d39560d71d38af8182ad6218d508c
Dave Reed <dave256@mac.com>
2019-10-19 16:00:46 -0400

    GuessTheFlag: Wrap up Challenge 1
    https://www.hackingwithswift.com/books/ios-swiftui/guess-the-flag-wrap-up

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
 
     @State private var showingScore = false
     @State private var scoreTitle = ""
-
+    @State private var score = 0
 
     var body: some View {
         ZStack {
@@ -47,7 +47,7 @@ struct ContentView: View {
             }
         }.alert(isPresented: $showingScore) {
             Alert(title: Text(scoreTitle), message:
-                Text("Your score is ???"),
+                Text("Your score is \(score)"),
                   dismissButton: .default(Text("Continue")) {
                     self.askQuestion()
                 })
@@ -57,6 +57,7 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
             scoreTitle = "Wrong"
         }
