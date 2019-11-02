583afd97c490002c44c3f9f2717950169535f434
Dave Reed <dave256@mac.com>
2019-11-02 18:49:15 -0400

    BetterRest: Cleaning up the user interface
    https://www.hackingwithswift.com/books/ios-swiftui/cleaning-up-the-user-interface

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
 
@@ -17,9 +17,16 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
 
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
                 Text("When do you want to wake up?")
                     .font(.headline)
 
@@ -28,20 +35,24 @@ struct ContentView: View {
                     // hide the please enter a time label
                     .labelsHidden()
 
-                Text("Desired amount of sleep")
-                    .font(.headline)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
 
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
                 }
 
-                Text("Daily coffee intake").font(.headline)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake").font(.headline)
 
-                Stepper(value: $coffeeAmount, in: 1...20) {
-                    if coffeeAmount == 1 {
-                        Text("1 cup")
-                    } else {
-                        Text("\(coffeeAmount) cups")
+                    Stepper(value: $coffeeAmount, in: 1...20) {
+                        if coffeeAmount == 1 {
+                            Text("1 cup")
+                        } else {
+                            Text("\(coffeeAmount) cups")
+                        }
                     }
                 }
             }
